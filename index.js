4e68c10e7760814a1c32af6a779682b067b0d61a
elVengador <dev.jimynicanor@gmail.com>
2023-12-03 16:11:33 -0500

    update vercel.json

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require("./app");
+const cors = require("cors")
 require("./database");
+
+app.use(cors());
 
 app.listen(app.get("PORT"), () => {
   console.log(`>. server listen on port ${app.get("PORT")}`);
